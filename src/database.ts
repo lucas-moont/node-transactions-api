7cc372d5c3185599ee7f3630bba15fd180be28e2
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-04-07 00:08:32 -0300

    chore: Production config.

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,23 @@
-import {knex as setupKnex, Knex} from 'knex'
-import { env } from './env'
+import { knex as setupKnex, Knex } from "knex";
+import { env } from "./env";
 
-if(!process.env.DATABASE_URL){
-  throw new Error('Database_URL env not found.')
+if (!process.env.DATABASE_URL) {
+  throw new Error("Database_URL env not found.");
 }
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL
-  },
+  client: "sqlite",
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? { 
+          filename: env.DATABASE_URL 
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
-    extension: 'ts',
-    directory: './db/migrations'
-  }
-}
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
 
-export const knex = setupKnex(config)
+export const knex = setupKnex(config);
